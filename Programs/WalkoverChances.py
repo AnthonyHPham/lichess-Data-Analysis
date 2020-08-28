3362e71e52476720ab54ff482b119cdbb855ec5b
Anthony H Pham <anthony.ha.pham@gmail.com>
2020-08-27 20:20:59 -0500

    Update pics and program

--- Programs/WalkoverChances.py
+++ Programs/WalkoverChances.py
@@ -2,7 +2,7 @@ import chess.pgn
 import matplotlib.pyplot as plt
 import numpy as np
 
-pgn_game = open("lichess_db_standard_rated_2013-01.pgn")
+pgn_game = open("lichess_db_standard_rated_2014-11.pgn")
 
 """
 Created by Anthony Ha-Anh Pham on July 26, 2020
@@ -25,36 +25,36 @@ def process_game():
         if game.headers["WhiteElo"] == "?" or game.headers["BlackElo"] == "?":
             a += 1
             if a % 1000 == 0:
-                print(f"Finished game #{a} out of 1,209,291 ({float(a)/1209291*100}%)")
+                print(f"Finished game #{a} out of 1,209,291 ({float(a) / 1209291 * 100}%)")
             game = chess.pgn.read_game(pgn_game)
             continue
-            
+
         game_rating_diff = int(game.headers["WhiteElo"]) - int(game.headers["BlackElo"])
-        index = 1 if game_rating_diff >= 0 else 0
+        index = 0 if game_rating_diff >= 0 else 1
 
         if 0 <= abs(game_rating_diff) <= 10:
-            if (game.headers["Result"] == "1-0" and game_rating_diff < 0) or \
-                    (game.headers["Result"] == "0-1" and game_rating_diff > 0):
+            if (game.headers["Result"] == "1-0" and game_rating_diff > 0) or \
+                    (game.headers["Result"] == "0-1" and game_rating_diff < 0):
                 rating_diff["1-10"][index] += 1
             rating_diff["1-10"][index + 2] += 1
         elif 11 <= abs(game_rating_diff) <= 25:
-            if (game.headers["Result"] == "1-0" and game_rating_diff < 0) or \
-                    (game.headers["Result"] == "0-1" and game_rating_diff > 0):
+            if (game.headers["Result"] == "1-0" and game_rating_diff > 0) or \
+                    (game.headers["Result"] == "0-1" and game_rating_diff < 0):
                 rating_diff["11-25"][index] += 1
             rating_diff["11-25"][index + 2] += 1
         elif 26 <= abs(game_rating_diff) <= 50:
-            if (game.headers["Result"] == "1-0" and game_rating_diff < 0) or \
-                    (game.headers["Result"] == "0-1" and game_rating_diff > 0):
+            if (game.headers["Result"] == "1-0" and game_rating_diff > 0) or \
+                    (game.headers["Result"] == "0-1" and game_rating_diff < 0):
                 rating_diff["26-50"][index] += 1
             rating_diff["26-50"][index + 2] += 1
         elif 51 <= abs(game_rating_diff) <= 99:
-            if (game.headers["Result"] == "1-0" and game_rating_diff < 0) or \
-                    (game.headers["Result"] == "0-1" and game_rating_diff > 0):
+            if (game.headers["Result"] == "1-0" and game_rating_diff > 0) or \
+                    (game.headers["Result"] == "0-1" and game_rating_diff < 0):
                 rating_diff["51-99"][index] += 1
             rating_diff["51-99"][index + 2] += 1
         elif 100 <= abs(game_rating_diff):
-            if (game.headers["Result"] == "1-0" and game_rating_diff < 0) or \
-                    (game.headers["Result"] == "0-1" and game_rating_diff > 0):
+            if (game.headers["Result"] == "1-0" and game_rating_diff > 0) or \
+                    (game.headers["Result"] == "0-1" and game_rating_diff < 0):
                 rating_diff["100+"][index] += 1
             rating_diff["100+"][index + 2] += 1
 
@@ -70,8 +70,8 @@ def process_game():
 def convert_rating_diff(rating_diff):
     for key in rating_diff.keys():
         values = rating_diff[key]
-        rating_diff[key] = [float(values[0])/max(1, values[2]) * 100, float(values[1])/max(1, values[3]) * 100]
-        
+        rating_diff[key] = [float(values[0]) / max(1, values[2]) * 100, float(values[1]) / max(1, values[3]) * 100]
+
     return rating_diff
 
 
@@ -87,18 +87,18 @@ def plot_upset_chances():
     black = [value[1] for value in rating_diff.values()]
 
     # Set position of bar on X axis
-    r1 = [x + (bar_width/2) for x in range(1, len(white)+1)]
+    r1 = [x + (bar_width / 2) for x in range(1, len(white) + 1)]
     r2 = [x + bar_width for x in r1]
 
     # Make the plot
     rects1 = plt.bar(r1, white, color="#deebf7", width=bar_width, edgecolor="white", label="White")
     rects2 = plt.bar(r2, black, color="#9ecae1", width=bar_width, edgecolor="white", label="Black")
 
-    plt.xticks([r + bar_width for r in range(1, len(white)+1)], ["1-10", "11-25", "25-50", "51-99", "100+"])
+    plt.xticks([r + bar_width for r in range(1, len(white) + 1)], ["1-10", "11-25", "25-50", "51-99", "100+"])
     plt.yticks(np.arange(0, 101, 10))
     plt.subplots_adjust(bottom=0.15)
 
-    plt.title("Chances of Winning if the Player has less ELO")
+    plt.title("Chances of Winning if the Player has more ELO")
     plt.xlabel("Rating Difference")
     plt.ylabel("Percentage")
 
